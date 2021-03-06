fe75a99f31d382e6744f1ba5b0f44f562d3849db
lisies93 <solorzanolisseth83@gmail.com>
2021-03-05 18:46:16 -0700

    first contoller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,8 +1,12 @@
 package com.skilldistillery.film.controllers;
 
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
 
@@ -12,11 +16,19 @@ public class FilmController {
 	@Autowired
 	private FilmDAO filmDAO;
 	
-	@RequestMapping(path= {"/", "home.do"})
-	public String home() {
-		// TODO -- add list of all films to model
-		
-		return "WEB-INF/views/index.jsp";
+		@RequestMapping( path = {"/","home.do"}, params = "id", method = RequestMethod.GET )
+		public ModelAndView findFilmById(int id) { 
+			ModelAndView mv = new ModelAndView();        
+			                                             
+			mv.setViewName("WEB-INF/views/index.jsp");
+			try {
+				mv.addObject("films", filmDAO.findFilmById(id));
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return mv;
 	}
 	
 	
